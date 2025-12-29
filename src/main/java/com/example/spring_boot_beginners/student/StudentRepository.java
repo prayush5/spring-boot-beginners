1ec2e16e4458188805753eb798cad5b3dc519bb0
Prayush <prayushestha5@gmail.com>
2025-12-29 14:52:44 +0545

    first commit

---
+++ src/main/java/com/example/spring_boot_beginners/student/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring_boot_beginners.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Student findByEmail(String email);
+
+    void deleteByEmail(String email);
+}
